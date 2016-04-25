3e28247ea4e6a30378ad02157422aa858e07184f
pandawan91 <vuongngo@outlook.de>
2016-04-26 01:36:21 +0200

    Update installJenkinsContainer.sh

--- installJenkinsContainer.sh
+++ installJenkinsContainer.sh
@@ -6,11 +6,12 @@ cd JenkinsContainer
 # build custom jenkins container
 sudo docker build -t $NAME .
 
+#create folder where the container stores 
 cd ..
-
-#make jenkins workspace 
-mkdir jenkins
-cd jenkins
+mkdir jenkins_home
+cd jenkins_home
 
 # start the container
 sudo docker run --name jenkins -d -p 8080:8080 -p 50000:50000 -v /$(pwd):/var/jenkins_home $NAME
+
+echo JenkinsContainer ready!!!
